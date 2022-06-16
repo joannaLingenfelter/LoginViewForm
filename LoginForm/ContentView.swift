d369f143f6192eb84ebf63a39bcd499c61b49612
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2022-06-16 15:19:59 -0400

    Initial Commit

---
+++ LoginForm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginForm
+//
+//  Created by Joanna Lingenfelter on 6/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginForm/LoginFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFormApp.swift
+//  LoginForm
+//
+//  Created by Joanna Lingenfelter on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
