b85cb6cd754c3ffab0fae4df22f49855b46bc973
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2022-06-16 20:21:08 -0400

    Use string errors instead of bool toggles

--- LoginForm/ContentView.swift
+++ LoginForm/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 protocol ErrorShowingTextFieldStyle: TextFieldStyle {
-    init(title: String, error: String)
+    init(title: String, error: String?)
 }
 
 struct ErrorTextField<TextFieldStyle: ErrorShowingTextFieldStyle>: View {
     typealias Body = TextField
 
     @Binding var text: String
-    @Binding var error: String
+    @Binding var error: String?
 
     let title: String
     let textFieldStyle: TextFieldStyle.Type
@@ -28,7 +28,7 @@ struct ErrorTextField<TextFieldStyle: ErrorShowingTextFieldStyle>: View {
 
 struct LabeledTextFieldStyle: ErrorShowingTextFieldStyle {
     let title: String
-    let error: String
+    let error: String?
 
     func _body(configuration: TextField<Self._Label>) -> some View {
         VStack(alignment: .leading) {
@@ -43,10 +43,12 @@ struct LabeledTextFieldStyle: ErrorShowingTextFieldStyle {
                 .frame(height: 1)
                 .background(Color.gray)
 
-            Text(error)
-                .foregroundColor(.red)
-                .opacity(error.isEmpty ? 0.0 : 1.0)
-                .font(.caption)
+            if let error = error {
+                Text(error)
+                    .foregroundColor(.red)
+                    .opacity(error.isEmpty ? 0.0 : 1.0)
+                    .font(.caption)
+            }
         }
     }
 }
@@ -64,58 +66,52 @@ struct ContentView: View {
     @FocusState private var focusedField: FormField?
     @State private var username: String = ""
     @State private var password: String = ""
-    @State private var showUsernameFocusError = false
-    @State private var showPasswordFocusError = false
+    @State private var usernameFocusError: String? = nil
+    @State private var passwordFocusError: String? = nil
+
+    @State var test: String = ""
 
     let errorText = "Required field"
 
     var body: some View {
         VStack() {
-            let userNameErrorBinding: Binding<String> = Binding {
-                showUsernameFocusError ? errorText : ""
-            } set: { _ in }
-
-            let passwordErrorBinding: Binding<String> = Binding {
-                showPasswordFocusError ? errorText : ""
-            } set: { _ in }
-
-            ErrorTextField(text: $username, error: userNameErrorBinding, title: FormField.username.title, textFieldStyle: LabeledTextFieldStyle.self)
+            ErrorTextField(text: $username, error: $usernameFocusError, title: FormField.username.title, textFieldStyle: LabeledTextFieldStyle.self)
                 .focused($focusedField, equals: .username)
                 .padding(.horizontal, 20)
                 .onChange(of: focusedField) { [focusedField] newValue in
                     if focusedField == nil {
                         // First change of focus
                         if newValue == .username {
-                            showUsernameFocusError = username.isEmpty
+                            usernameFocusError = username.isEmpty ? errorText : nil
                         } else if newValue == .password {
-                            showPasswordFocusError = password.isEmpty
+                            passwordFocusError = password.isEmpty ? errorText : nil
                         }
                     } else {
-                        showUsernameFocusError = username.isEmpty
-                        showPasswordFocusError = password.isEmpty
+                        usernameFocusError = username.isEmpty ? errorText : nil
+                        passwordFocusError = password.isEmpty ? errorText : nil
                     }
                 }
 
-            ErrorTextField(text: $password, error: passwordErrorBinding, title: FormField.password.title, textFieldStyle: LabeledTextFieldStyle.self)
+            ErrorTextField(text: $password, error: $passwordFocusError, title: FormField.password.title, textFieldStyle: LabeledTextFieldStyle.self)
                 .focused($focusedField, equals: .password)
                 .padding(.horizontal, 20)
                 .onChange(of: focusedField) { [focusedField] newValue in
                     if focusedField == nil {
                         // First change of focus
                         if newValue == .password {
-                            showPasswordFocusError = password.isEmpty
+                            passwordFocusError = password.isEmpty ? errorText : nil
                         } else if newValue == .username {
-                            showUsernameFocusError = username.isEmpty
+                            usernameFocusError = username.isEmpty ? errorText : nil
                         }
                     } else {
-                        showUsernameFocusError = username.isEmpty
-                        showPasswordFocusError = password.isEmpty
+                        usernameFocusError = username.isEmpty ? errorText : nil
+                        passwordFocusError = password.isEmpty ? errorText : nil
                     }
                 }
             Spacer(minLength: 20)
             Button {
-                showUsernameFocusError = username.isEmpty
-                showPasswordFocusError = password.isEmpty
+                usernameFocusError = username.isEmpty ? errorText : nil
+                passwordFocusError = password.isEmpty ? errorText : nil
             } label: {
                 Text("Login")
             }
@@ -128,8 +124,8 @@ struct ContentView: View {
         }
         .fixedSize(horizontal: false, vertical: true)
         .onSubmit {
-            showUsernameFocusError = username.isEmpty
-            showPasswordFocusError = password.isEmpty
+            usernameFocusError = username.isEmpty ? errorText : nil
+            passwordFocusError = password.isEmpty ? errorText : nil
             
             if focusedField == .username {
                 focusedField = .password
