66ef1aa4cc9ff60780a2cc8449e310ce0e4dc084
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2022-06-17 08:58:41 -0400

    Rename LoginView

--- LoginForm/LoginFormApp.swift
+++ LoginForm/LoginFormApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LoginFormApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
--- LoginForm/LoginView.swift
+++ LoginForm/LoginView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LoginView.swift
 //  LoginForm
 //
 //  Created by Joanna Lingenfelter on 6/16/22.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LoginView: View {
     enum FormField: String, CaseIterable {
         case username
         case password
@@ -90,6 +90,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LoginView()
     }
 }
